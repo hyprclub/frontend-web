18f28a937967fb61c0d971c447a4c141464c40fa
Shivam Sharma <sharma.shivamhr@gmail.com>
2022-01-02 12:55:54 +0530

    ui fixes in Profile

--- frontend-hyprclub/src/components/item.js
+++ frontend-hyprclub/src/components/item.js
@@ -2,19 +2,19 @@ export const Iitems = [
     {
         title: "European themed Collage",
         desc: "Classic collage capturing the essence of Europian fashion.",
-        price: "10,753",
+        price: "INR 10,753",
         image: "images/createdImg.png"
       },
       {
         title: "European themed Collage",
         desc: "Classic collage capturing the essence of Europian fashion.",
-        price: "10,753",
+        price: "INR 10,753",
         image: "images/createdImg.png"
       },
       {
         title: "European themed Collage",
         desc: "orem Ipsum is simply dummy text of the printing and typesetting industry.",
-        price: "10,753",
+        price: " INR 10,753",
         image: "images/createdImg.png"
       }
   ];
@@ -23,17 +23,17 @@ export const Iitems = [
   export const storeItems = [
     {
       title: "Lightroom Preset Bundle 01-27",
-      price: "10,753",
+      price: "INR 10,753",
       image: "images/storeImg.png"
     },
     {
       title: "Lightroom Preset Bundle 01-27",
-      price: "10,753",
+      price: "INR 10,753",
       image: "images/storeImg.png"
     },
     {
       title: "Lightroom Preset Bundle 01-27",
-      price: "10,753",
+      price: "INR 10,753",
       image: "images/storeImg.png"
     },
     
