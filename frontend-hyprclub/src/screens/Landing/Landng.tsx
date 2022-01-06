805c4670cb351933069b0ae3be635089fee9cdd8
PrathamSharma1510 <62831943+PrathamSharma1510@users.noreply.github.com>
2022-01-07 01:10:53 +0530

    landing change

--- frontend-hyprclub/src/screens/Landing/Landng.tsx
+++ frontend-hyprclub/src/screens/Landing/Landng.tsx
@@ -8,14 +8,30 @@ import BG_STARS from "./BG_STARS.svg";
 const Landing = () => {
   return (
     <>
-    <Header_login></Header_login>
-    {/* style={{ backgroundImage: `url(${BG_STARS})` }} */}
-      <div className={styles.part1}  >
-        <div className={styles.hypr}><span><a>HYPRCLUB</a>&nbsp;PRESENTS</span></div>
-        <div className={styles.Revolution}>The Next <span>Social <br></br> Revolution</span></div>
-        <div className={styles.SocialNetwork}>A Social Network where Creator Economy<br></br> meets the Metaverse.</div>
-        <Button className={styles.postBtna}> <span className={styles.PostbtnTexta}>Get Early Access</span></Button>
-        {/* <PostButton btnText='Get Early Access' small={false}></PostButton></div> */}
+      <div className={styles.part1}>
+        <Header_login></Header_login>
+        {/* style={{ backgroundImage: `url(${BG_STARS})` }} */}
+        <div>
+          <div className={styles.hypr}>
+            <span>
+              <a>HYPRCLUB</a>&nbsp;PRESENTS
+            </span>
+          </div>
+          <div className={styles.Revolution}>
+            The Next{" "}
+            <span>
+              Social <br></br> Revolution
+            </span>
+          </div>
+          <div className={styles.SocialNetwork}>
+            A Social Network where Creator Economy<br></br> meets the Metaverse.
+          </div>
+          <Button className={styles.postBtna}>
+            {" "}
+            <span className={styles.PostbtnTexta}>Get Early Access</span>
+          </Button>
+          {/* <PostButton btnText='Get Early Access' small={false}></PostButton></div> */}
+        </div>
       </div>
     </>
   );
