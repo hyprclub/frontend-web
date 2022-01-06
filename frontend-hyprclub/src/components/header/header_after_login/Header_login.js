9ddb757a67c67c3de042aa3d74d98d1245351d4d
Shivam Sharma <sharma.shivamhr@gmail.com>
2022-01-06 20:58:44 +0530

    header changed

--- frontend-hyprclub/src/components/header/header_after_login/Header_login.js
+++ frontend-hyprclub/src/components/header/header_after_login/Header_login.js
@@ -73,7 +73,7 @@ const nav = [
               className={cn("button-small", styles.button)}
               to="/upload-variants"
             >
-              Upload
+
             </Link>
           </div>
         <Upload_Button/>
