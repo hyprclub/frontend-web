468a8850c05ffd1d5f15a105cd3af375d3365f37
PrathamSharma1510 <62831943+PrathamSharma1510@users.noreply.github.com>
2022-01-07 20:43:37 +0530

    landing

--- frontend-hyprclub/src/screens/Landing/Landng.tsx
+++ frontend-hyprclub/src/screens/Landing/Landng.tsx
@@ -15,7 +15,7 @@ const Landing = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
     pauseOnHover: true,
   };
   return (
