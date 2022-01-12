599c39b4e4e6bafc0ebaad09f1c9a57cb56f8fcb
Shivam Sharma <sharma.shivamhr@gmail.com>
2022-01-12 20:37:39 +0530

    Hypr <Logo> routing

--- frontend-hyprclub/src/components/logo/Logo.tsx
+++ frontend-hyprclub/src/components/logo/Logo.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './logo.css'
+import './logo.css';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
     return (
         <div className='p-4'>
-            <img className='logoImg' src="images/logo.png" alt="" />
+        <Link to='/'>
+         <img className='logoImg' src="images/logo.png" alt="" />
+        </Link>
+
         </div>
     )
 }
