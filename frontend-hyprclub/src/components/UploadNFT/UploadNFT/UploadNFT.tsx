8c8384bb68babe93b5acc4d5c3b6fa5e22667487
Antiarin <73146875+Antiarin@users.noreply.github.com>
2022-01-19 19:23:14 +0530

    add perk button in upload nft made
    add perk button in upload nft  made

--- frontend-hyprclub/src/components/UploadNFT/UploadNFT/UploadNFT.tsx
+++ frontend-hyprclub/src/components/UploadNFT/UploadNFT/UploadNFT.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./UploadNFT.module.css";
 import { FileUploader } from "react-drag-drop-files";
 import ToggleBtn from "./ToggleBtn/ToggleBtn";
@@ -7,6 +7,7 @@ import { FileArrowUp, Plus } from "phosphor-react";
 import GradBorder from "../../../screens/NFTs/GradBorder/GradBorder";
 import { Form } from "react-bootstrap";
 import InputField from "../../inputField/Input";
+import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 
 
 const fileTypes = ["GIF", "PNG", "WEBP", "MP4", "MP3"];
@@ -33,26 +34,43 @@ const items = [
 
 const UploadNFT = () => {
     const [category, setCategory] = useState(Categories[0]);
-    const [input, SetInput] = useState("")
+    const [perks, setPerks]: any[] = useState([])
     const [file, setFile] = useState(null);
-    const handleChange = (file: any) => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    // console.log(perks);
+    const addPerk = () => {
+        if (inputRef!.current!.value === "") return;
+        setPerks([...perks, inputRef!.current!.value]);
+        inputRef!.current!.value = "";
+    }
+    const enterPerk=(e:any)=>{
+        if(e.key==='Enter') return addPerk();
+
+    }
+    // const Perkdiv=()=>{
+    //     <>
+    //     </>
+    // }
+
+    const handleChange = () => {
         setFile(file);
     };
     return (
         <>
             <div className={styles.wrapper}>
                 <div className={styles.section}>
-                    <div className={styles.title}> Create an NFT</div>
+                    <h1 className={styles.title}> Create an NFT</h1>
                     <div className={styles.Upload}>
-                        <span className={styles.upldFile}>Upload file</span>
-                        <span className={styles.drag}>Drag or choose your file to upload</span>
+                        <h4 className={styles.upldFile}>Upload file</h4>
+                        <h6 className={styles.drag}>Drag or choose your file to upload</h6>
                         <FileUploader handleChange={handleChange} name="file" types={fileTypes} >
                             <FileArrowUp size={24} id={styles.Filearrow} />
-                            <span className={styles.fileTypes}>PNG, GIF, WEBP, MP4 or MP3. Max 1Gb.</span>
+                            <h6 className={styles.fileTypes}>PNG, GIF, WEBP, MP4 or MP3. Max 1Gb.</h6>
                         </FileUploader>
                     </div>
                     <div className={styles.ItemName}>
-                        {/* <span className={styles.Heading}>Enter Item Name</span> */}
+                        {/* <h6 className={styles.Heading}>Enter Item Name</h6> */}
                         <InputField
                             typeOfInput="text"
                             lableText="ENTER ITEM NAME"
@@ -79,26 +97,32 @@ const UploadNFT = () => {
                         </div>
                     </div>
                     <div className={styles.ItemDesc}>
-                        <span className={styles.Heading}>Enter Item Description</span>
+                        <h6 className={styles.Heading}>Enter Item Description</h6>
                         <textarea className={styles.Input_Description} />
-                        <span className={styles.note}>Use 30-500 characters </span>
+                        <h6 className={styles.note}>Use 30-500 characters </h6>
                     </div>
                     <div className={styles.Perks}>
                         <div className={styles.PerkHead}>
-                            <span className={styles.Heading}>ADD Perk</span>
+                            <h6 className={styles.Heading}>ADD Perk</h6>
                             <div className={styles.transferable}>
-                                <span className={styles.Heading_transfer}>Make Transferable</span>
+                                <h6 className={styles.Heading_transfer}>Make Transferable</h6>
                                 <ToggleBtn />
                             </div>
                         </div>
-                        <input type="text" className={styles.Input} onChange={() => console.log} />
+                        <input type="text" onKeyDown={enterPerk} className={styles.Input} ref={inputRef} />
                         <div className={styles.PerkBody}>
-                            <div className={styles.Plus}><Plus size={32} /></div>
-                            <span className={styles.AddPerk}>Add Perk</span>
+                            <button onClick={addPerk} className={styles.Plus}><Plus size={20} className={styles.hh} /></button>
+                            <h5 className={styles.AddPerk}>Add Perk</h5>
+                            {perks && perks.map((e: any) => (
+                                <>
+                                    <div className={styles.Plus} />
+                                    <h6 className={styles.AddPerk}>{e}</h6>
+                                </>
+                            ))}
                         </div>
                     </div>
                     <div className={styles.dropdown}>
-                        <span className={styles.Heading}>Category</span>
+                        <h6 className={styles.Heading}>Category</h6>
                         <Form.Select aria-label="Default select example" className={styles.drop}>
                             {Categories.map((x, index) => (
                                 <option key={index}>{x} </option>
@@ -109,12 +133,12 @@ const UploadNFT = () => {
                 <div className={styles.Bottom}>
                     <div className={styles.Collection}>
                         <h3 className={styles.Collection_title}>Choose Collection</h3>
-                        <span className={styles.Collection_sub}>Choose an existing collection or create a new one</span>
+                        <h6 className={styles.Collection_sub}>Choose an existing collection or create a new one</h6>
                         <Collection item={items} />
                     </div>
                     <div className={styles.btns}>
                         <GradBorder className={styles.Gradbtn} text="Create NFT" />
-                        <button type="submit" className={styles.Previewbtn}><span className={styles.btnText}> Preview NFT</span></button>
+                        <button type="submit" className={styles.Previewbtn}><p className={styles.btnText}> Preview NFT</p></button>
                     </div>
                 </div>
             </div>
