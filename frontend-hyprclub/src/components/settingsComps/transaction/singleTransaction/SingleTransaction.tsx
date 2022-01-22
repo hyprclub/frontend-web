c3ea41a839e2fc0675edccfd68082927cb9832d0
Shivam Sharma <sharma.shivamhr@gmail.com>
2022-01-22 21:09:01 +0530

    transaction page (responsiveness for desktop left)

--- frontend-hyprclub/src/components/settingsComps/transaction/singleTransaction/SingleTransaction.tsx
+++ frontend-hyprclub/src/components/settingsComps/transaction/singleTransaction/SingleTransaction.tsx
@@ -1,28 +1,36 @@
 import clsx from 'clsx';
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styles from './singleTransaction.module.css';
 
+
 interface Transaction {
-    purchased?: boolean
+    success?: boolean
     NFTname: string
+    transactionID: string
     date: Date | string
-    username: string
+    price: string
+
 }
 
-const SingleTransaction = ({purchased, NFTname, date, username}: Transaction) => {
+const SingleTransaction = ({success, NFTname, transactionID, date, price}: Transaction) => {
     return (
         <>
             <div className={styles.mainDiv}>
-                <div className='d-flex  px-4 py-3 align-items-center'>
-                        <img src="/images/pfImage.png" alt="" className={styles.pfImage} />
+                <div className='d-flex  px-5 py-2 align-items-center'>
+                        {/* <img src="/images/pfImage.png" alt="" className={styles.pfImage} /> */}
                     <div className={clsx('d-flex justify-content-between ', styles.nameUsernamePrice)}>
                         <div className={styles.nameAndPrice}>
-                            <p className={styles.boldTitle}>{purchased ? "Purchased" : 'Your' } NFT - {NFTname}{!purchased && " was Purchased!"}</p>
+                        <Link to='#'><p className={styles.boldTitle}>{NFTname}</p></Link>
+                            <p className={styles.boldTitle}>{transactionID}</p>
                             <p className={styles.date}>{date}</p>
                         </div>
-                    
-                        <p className={styles.username}>@{username}</p>
-                        <p className={styles.price}>10,475 INR</p>
+
+                        <Link to='#'><li className={styles.boldTitleDesk}>{NFTname}</li></Link>
+                        <p className={styles.dateDesk}>{date}</p>
+                        <p className={styles.transDesk}>{transactionID}</p>
+                        <li className={styles.statusDesk}>{success ? "🟢 Success" : "🔴 Failed" }</li>
+                        <p className={styles.price}><span className={styles.statusMob}>{success ? "🟢" : "🔴" }</span> INR {price}</p>
                     </div>
                 </div>
             </div>
