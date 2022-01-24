5b9b88f015bb686c2c7aa7b993e15b067fd61108
Shivam Sharma <sharma.shivamhr@gmail.com>
2022-01-24 19:11:27 +0530

    recent transaction component (dashboard)

---
+++ frontend-hyprclub/src/components/creatorDashboard/TransactionHistory.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./transactionHistory.module.css";
+import TransactionSingle from "./transactionSingle/TransactionSingle";
+const TransactionHistory = () => {
+  return (
+    <>
+      <div className={styles.mainDiv}>
+        <h2 className={styles.heading}>Recent Transactions</h2>
+          <div className={styles.content}>
+          <TransactionSingle
+            price="15,560"
+            transactionID="HYPR9459207532"
+            date={"14/01/2022"}
+            NFTname="Homegrown Tulips"
+          />
+          </div>
+          <div className={styles.content}>
+          <TransactionSingle
+            price="15,560"
+            transactionID="HYPR9459207532"
+            date={"14/01/2022"}
+            NFTname="Homegrown Tulips"
+            success
+          />
+          </div>
+          <div>
+          <TransactionSingle
+            price="15,560"
+            transactionID="HYPR9459207532"
+            date={"14/01/2022"}
+            NFTname="Homegrown Tulips"
+            success
+          />
+          </div>
+
+      </div>
+    </>
+  );
+};
+
+export default TransactionHistory;
