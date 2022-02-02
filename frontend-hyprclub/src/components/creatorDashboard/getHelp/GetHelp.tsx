299e5b9ce05886d3ba45efe7222053282ce0148e
archit0712 <57456294+archit0712@users.noreply.github.com>
2022-02-02 22:12:51 +0530

    feat : get help done

--- frontend-hyprclub/src/components/creatorDashboard/getHelp/GetHelp.tsx
+++ frontend-hyprclub/src/components/creatorDashboard/getHelp/GetHelp.tsx
@@ -7,23 +7,25 @@ import { firebaseApp } from "../../../firebaseConfig";
 import GradientBorder from "../../gradientBorderBtn/GradientBorder";
 import styles from "./getHelp.module.css";
 
-const GetHelp = ({className}:any) => {
+const GetHelp = ({ className }: any) => {
   const [message, setMessage] = useState("");
   const userData = useSelector((state: RootStateOrAny) => state.userData);
   const makeContactUsId = (len: number) => {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const characterLengths = characters.length;
     for (let i = 0; i < len; i++) {
       result += characters.charAt(Math.floor(Math.random() * characterLengths));
     }
     return result;
-  }
+  };
   const handleSubmit = async () => {
     const db = getFirestore(firebaseApp);
     const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth() + 1
-      }/${current.getFullYear()}`;
+    const date = `${current.getDate()}/${
+      current.getMonth() + 1
+    }/${current.getFullYear()}`;
     const contactUsId = "contactUs_id_" + makeContactUsId(26);
     await setDoc(doc(db, "contactus", contactUsId), {
       name: userData?.name,
@@ -39,31 +41,43 @@ const GetHelp = ({className}:any) => {
       .catch((error) => {
         console.log(error);
       });
+    console.log(userData);
   };
   return (
     <>
       <div className={clsx(styles.mainDiv, className)}>
         <h1 className={styles.heading}>Get Help</h1>
         <div className={clsx(styles.content)}>
-            <p className={styles.ques}>Got any questions? Check out our <Link to='#'><span>FAQ page.</span></Link></p>
-            <p className={styles.ques}>Couldn’t find your query? Ask us here!</p>
+          <p className={styles.ques}>
+            Got any questions? Check out our{" "}
+            <Link to="#">
+              <span>FAQ page.</span>
+            </Link>
+          </p>
+          <p className={styles.ques}>Couldn’t find your query? Ask us here!</p>
 
-            <label className={styles.lableText} htmlFor="exampleFormControlTextarea1">ASK YOUR QUERY</label>
-            <textarea 
-              className={clsx("form-control", styles.textarea)} 
-              id="exampleFormControlTextarea1" rows={5}
-              onChange={(e: React.ChangeEvent<any>) => {
-                setMessage(e.target.value);
+          <label
+            className={styles.lableText}
+            htmlFor="exampleFormControlTextarea1"
+          >
+            ASK YOUR QUERY
+          </label>
+          <textarea
+            className={clsx("form-control", styles.textarea)}
+            id="exampleFormControlTextarea1"
+            rows={5}
+            onChange={(e: React.ChangeEvent<any>) => {
+              setMessage(e.target.value);
+            }}
+          ></textarea>
+          <div className={clsx("text-center d-flex", styles.avt)}>
+            <GradientBorder
+              text="Submit"
+              onClick={(e: React.ChangeEvent<HTMLInputElement>) => {
+                handleSubmit();
               }}
-            ></textarea>
-            <div className={clsx('text-center d-flex',styles.avt)}>
-                        <GradientBorder 
-                          text='Submit'
-                          onClick={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            handleSubmit();
-                          }}
-                        />
-            </div>
+            />
+          </div>
         </div>
       </div>
     </>
--- frontend-hyprclub/src/screens/Creator/Creator.tsx
+++ frontend-hyprclub/src/screens/Creator/Creator.tsx
@@ -1,48 +1,50 @@
-import clsx from 'clsx';
-import React , {useEffect} from 'react';
-import CreatorStats from '../../components/creatorDashboard/creatorStats/CreatorStats';
-import GetHelp from '../../components/creatorDashboard/getHelp/GetHelp';
-import MyNfts from '../../components/creatorDashboard/myNFTs/MyNfts';
-import SayThanks from '../../components/creatorDashboard/sayThanks/SayThanks';
-import Perks from '../../components/creatorDashboard/Perks/Perks';
-import TransactionHistory from '../../components/creatorDashboard/TransactionHistory';
-import Header_login from '../../components/header/header_after_login/Header_login';
-import styles from './styles.module.css'
-import { RootStateOrAny, useSelector } from 'react-redux';
-import SalesChart from '../../components/creatorDashboard/charts/SalesChart';
+import clsx from "clsx";
+import React, { useEffect } from "react";
+import CreatorStats from "../../components/creatorDashboard/creatorStats/CreatorStats";
+import GetHelp from "../../components/creatorDashboard/getHelp/GetHelp";
+import MyNfts from "../../components/creatorDashboard/myNFTs/MyNfts";
+import SayThanks from "../../components/creatorDashboard/sayThanks/SayThanks";
+import Perks from "../../components/creatorDashboard/Perks/Perks";
+import TransactionHistory from "../../components/creatorDashboard/TransactionHistory";
+import Header_login from "../../components/header/header_after_login/Header_login";
+import styles from "./styles.module.css";
+import { useNavigate } from "react-router";
+import { RootStateOrAny, useSelector } from "react-redux";
+import SalesChart from "../../components/creatorDashboard/charts/SalesChart";
 const Creator = () => {
-    const userData = useSelector((state : RootStateOrAny) => state.userData);
-    const {loggedIn , uid} = useSelector((state : RootStateOrAny) => state.userData);
-
-    useEffect(()=>{
-        if(userData.isCreator && loggedIn && uid){
-
-        }else{
-            console.log("Looks like your are not a creator");
-        }
-
-    },[userData , loggedIn , uid])
-  return <>
-        <div className={styles.body}>
-            <Header_login/>
-            <div className='container'>
-                    <CreatorStats/>
-                <div className={clsx('mt-4 pt-4 row', styles.mainDiv)}>
-                    <div className='col-lg-8'>
-                        <SalesChart/>
-                        <MyNfts/>
-                        <Perks/>
-                        <SayThanks/>
-                    </div>
-                    <div className='col-lg-4  mt-4 pt-4'>
-                        <TransactionHistory/>
-                        <GetHelp className='mt-5'/>
-                    </div>
-
-                </div>
+  const userData = useSelector((state: RootStateOrAny) => state.userData);
+  const { loggedIn, uid } = useSelector(
+    (state: RootStateOrAny) => state.userData
+  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (userData?.isCreator && loggedIn && uid) {
+    } else {
+      navigate("/" + userData?.username);
+    }
+  }, [userData, loggedIn, uid]);
+  return (
+    <>
+      <div className={styles.body}>
+        <Header_login />
+        <div className="container">
+          <CreatorStats />
+          <div className={clsx("mt-4 pt-4 row", styles.mainDiv)}>
+            <div className="col-lg-8">
+              <SalesChart />
+              <MyNfts />
+              <Perks />
+              <SayThanks />
             </div>
+            <div className="col-lg-4  mt-4 pt-4">
+              <TransactionHistory />
+              <GetHelp className="mt-5" />
+            </div>
+          </div>
         </div>
-  </>;
+      </div>
+    </>
+  );
 };
 
 export default Creator;
