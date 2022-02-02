0e7b55440a17d151cbdf11d9bbdbd8c0e5bda5eb
archit0712 <57456294+archit0712@users.noreply.github.com>
2022-02-02 18:44:12 +0530

    feat : forgot password

--- frontend-hyprclub/src/components/settingsComps/privacyAndSec/Privacy.tsx
+++ frontend-hyprclub/src/components/settingsComps/privacyAndSec/Privacy.tsx
@@ -1,40 +1,74 @@
-import clsx from 'clsx';
-import React from 'react'
-import GradientBorder from '../../gradientBorderBtn/GradientBorder';
-import InputField from '../../inputField/Input';
-import Switch from '../../switch/Switch';
-import styles from './privacy.module.css';
+import clsx from "clsx";
+import React, { useState } from "react";
+import GradientBorder from "../../gradientBorderBtn/GradientBorder";
+import InputField from "../../inputField/Input";
+import Switch from "../../switch/Switch";
+import styles from "./privacy.module.css";
+import { firebaseApp } from "../../../firebaseConfig";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 const Privacy = () => {
-    return (
-        <>
-           <div className={styles.mainDiv}>
-                <h2 className={styles.title}>Privacy & Security</h2>
-                <div className={styles.content}>
-                    <div className={styles.private}>
-                        <h3 className={styles.heading}>Private Account</h3>
-                        <div className={clsx('d-flex align-items-center', styles.enablepvtdiv)}>
-                            <p className={styles.enableprivate}>Enable private account</p>
-                            <Switch/>
-                        </div>
-                        <p className={styles.desc}>By enabling private account, your posts can only be viewed by your followers and the accounts that you approve. Follower requests can be accessed through Notifications.</p>
-                    </div>
+  const [resetEmail, setResetEmail] = useState("");
+  const auth = getAuth(firebaseApp);
+  const __DEV__ = document.domain === "localhost";
+  const updateState = (e: React.ChangeEvent<any>) => {
+    setResetEmail(e.target.value);
+  };
 
-                    <div className={styles.changePs}>
-                        <h3 className={styles.heading}>Change Password</h3>
-                        <div className={clsx('d-flex mb-2', styles.pass)}>
-                            <InputField placeholder={'example@hyprclub.com'} garyBold lableText='ENTER REGISTERED EMAIL' typeOfInput='text'/>
-                        </div>
-                        <p className={styles.descPass}>A password reset link will be sent to your registered email address.</p>
-                        <div className={clsx('col-md-3 text-center d-flex',styles.avt)}>
-                         <GradientBorder text='Save Changes'/>
-                    </div>
+  const resetPassword = async () => {
+    sendPasswordResetEmail(auth, resetEmail, {
+      url: __DEV__
+        ? "http://localhost:3000/login"
+        : "http://hyprclub.com/login",
+    });
+    console.log("Email sent");
+  };
+  return (
+    <>
+      <div className={styles.mainDiv}>
+        <h2 className={styles.title}>Privacy & Security</h2>
+        <div className={styles.content}>
+          <div className={styles.private}>
+            <h3 className={styles.heading}>Private Account</h3>
+            <div
+              className={clsx("d-flex align-items-center", styles.enablepvtdiv)}
+            >
+              <p className={styles.enableprivate}>Enable private account</p>
+              <Switch />
+            </div>
+            <p className={styles.desc}>
+              By enabling private account, your posts can only be viewed by your
+              followers and the accounts that you approve. Follower requests can
+              be accessed through Notifications.
+            </p>
+          </div>
 
-                    </div>
-                </div>
-           </div> 
-        </>
-    )
-}
+          <div className={styles.changePs}>
+            <h3 className={styles.heading}>Change Password</h3>
+            <div className={clsx("d-flex mb-2", styles.pass)}>
+              <InputField
+                placeholder={"example@hyprclub.com"}
+                name="email"
+                garyBold
+                lableText="ENTER REGISTERED EMAIL"
+                typeOfInput="text"
+                onChange={(e: React.ChangeEvent<any>) => {
+                  updateState(e);
+                }}
+              />
+            </div>
+            <p className={styles.descPass}>
+              A password reset link will be sent to your registered email
+              address.
+            </p>
+            <div className={clsx("col-md-3 text-center d-flex", styles.avt)}>
+              <GradientBorder onClick={resetPassword} text="Send Email" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Privacy
+export default Privacy;
