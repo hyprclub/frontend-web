1c03bd4392cf908eac22dc36aabece5f2c7658c9
archit0712 <57456294+archit0712@users.noreply.github.com>
2022-02-10 23:50:30 +0530

    feat : transactionHistory

--- frontend-hyprclub/src/components/settingsComps/transaction/Transaction.tsx
+++ frontend-hyprclub/src/components/settingsComps/transaction/Transaction.tsx
@@ -1,70 +1,85 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleTransaction from "./singleTransaction/SingleTransaction";
 import styles from "./transaction.module.css";
+import { db } from "../../../firebaseConfig";
+import {
+  getFirestore,
+  query,
+  where,
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+} from "firebase/firestore";
+import { RootStateOrAny, useSelector } from "react-redux";
 
 const Transaction = () => {
+  const [transactionData, setTransactionData] = useState<any>([]);
+  const { loggedIn, uid } = useSelector(
+    (state: RootStateOrAny) => state?.userData
+  );
+  const userData = useSelector((state: RootStateOrAny) => state?.userData);
+
+  useEffect(() => {
+    const run = async () => {
+      let transactions: any = [];
+      if (uid && loggedIn) {
+        // console.log(uid);
+        // console.log(loggedIn);
+        await getDocs(
+          query(collection(db, "paymentRecords"), where("buyerUID", "==", uid))
+        )
+          .then((snapshot) => {
+            snapshot.forEach((docs) => {
+              if (docs.exists()) {
+                transactions.push(docs.data());
+              } else {
+                transactions.push("Null");
+              }
+            });
+            console.log(transactions);
+            setTransactionData(transactions);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        // console.log(transactions[0].amount)
+      } else {
+        console.log("Not Logged In");
+      }
+    };
+    // console.log(transactionData[0].amount)
+
+    run();
+  }, [uid, loggedIn]);
   return (
     <>
       <div className={styles.mainDiv}>
         <h2 className={styles.title}>Transaction History</h2>
         <div className={styles.content}>
-        <div className={clsx("container",styles.tranInfo)}>
-            <div className={clsx("row",styles.row)}>
-              <div className={clsx("col-sm",styles.contP)}>Product</div>
-              <div className={clsx("col-sm",styles.contD)}>Date</div>
-              <div className={clsx("col-sm",styles.contT)}>Transaction ID</div>
-              <div className={clsx("col-sm",styles.contO)}><p>Item Type</p></div>
-              <div className={clsx("col-sm ",styles.contS)}>Status</div>
-              <div className={clsx("col-sm",styles.contO)}><p>Order Total</p></div>
+          <div className={clsx("container", styles.tranInfo)}>
+            <div className={clsx("row", styles.row)}>
+              <div className={clsx("col-sm", styles.contP)}>Product</div>
+              <div className={clsx("col-sm", styles.contD)}>Date</div>
+              <div className={clsx("col-sm", styles.contT)}>Transaction ID</div>
+              <div className={clsx("col-sm ", styles.contS)}>Status</div>
+              <div className={clsx("col-sm", styles.contO)}>
+                <p>Order Total</p>
+              </div>
             </div>
           </div>
 
-          <SingleTransaction
-            price="10,475"
-            transactionID="HYPR9459207532"
-            transactionType="SayThanks"
-            date={"14/01/2022"}
-            success
-            NFTname="Ethereal Skies"
-          />
-          <SingleTransaction
-            price="15,560"
-            transactionID="HYPR9459207532"
-            transactionType="SayThanks"
-            date={"14/01/2022"}
-            NFTname="Homegrown Tulips"
-          />
-          <SingleTransaction
-            price="10,475"
-            transactionID="HYPR9459207532"
-            transactionType="SayThanks"
-            date={"14/01/2022"}
-            success
-            NFTname="Ethereal Skies"
-          />
-          <SingleTransaction
-            price="15,560"
-            transactionID="HYPR9459207532"
-            transactionType="SayThanks"
-            date={"14/01/2022"}
-            NFTname="Homegrown Tulips"
-          />
-          <SingleTransaction
-            price="10,475"
-            transactionID="HYPR9459207532"
-            transactionType="SayThanks"
-            date={"14/01/2022"}
-            success
-            NFTname="Ethereal Skies"
-          />
-          <SingleTransaction
-            price="15,560"
-            transactionID="HYPR9459207532"
-            transactionType="SayThanks"
-            date={"14/01/2022"}
-            NFTname="Homegrown Tulips"
-          />
+          {transactionData.map((e: any, index: number) => (
+            <SingleTransaction
+              price={`${e.amount}`}
+              transactionID={e.razorpayPaymentId}
+              date={e.timestamp.slice(4, 15)}
+              success={e.transactionSuccess}
+              nftUid={e.purchasedNftUID}
+              itemName={e.transactionType}
+            />
+          ))}
         </div>
 
         <div className="d-flex justify-content-center mt-5">
--- frontend-hyprclub/src/components/settingsComps/transaction/singleTransaction/SingleTransaction.tsx
+++ frontend-hyprclub/src/components/settingsComps/transaction/singleTransaction/SingleTransaction.tsx
@@ -1,43 +1,73 @@
-import clsx from 'clsx';
-import React ,{useEffect} from 'react'
-import { Link } from 'react-router-dom';
-import styles from './singleTransaction.module.css';
-
+import clsx from "clsx";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "./singleTransaction.module.css";
 
 interface Transaction {
-    success?: boolean
-    NFTname: string
-    transactionID: string
-    date: Date | string
-    price: string
-    transactionType : string
-
+  success?: any;
+  itemName: string;
+  transactionID: string;
+  date: Date | string;
+  price: string;
+  nftUid: any;
 }
 
-const SingleTransaction = ({success, NFTname, transactionID, date, price, transactionType}: Transaction) => {
-    return (
-        <>
-            <div className={styles.mainDiv}>
-                <div className='d-flex  px-5 py-2 align-items-center'>
-                        {/* <img src="/images/pfImage.png" alt="" className={styles.pfImage} /> */}
-                    <div className={clsx('d-flex justify-content-between ', styles.nameUsernamePrice)}>
-                        <div className={styles.nameAndPrice}>
-                        <Link to='#'><p className={styles.boldTitle}>{NFTname}</p></Link>
-                            <p className={styles.boldTitle}>{transactionID}</p>
-                            <p className={styles.date}>{date}</p>
-                        </div>
-
-                        <Link to='#'><li className={styles.boldTitleDesk}>{NFTname}</li></Link>
-                        <p className={styles.dateDesk}>{date}</p>
-                        <p className={styles.transDesk}>{transactionID}</p>
-                        <p className={styles.transDesk}>{transactionType}</p>
-                        <li className={styles.statusDesk}>{success ? "🟢 Success" : "🔴 Failed" }</li>
-                        <p className={styles.price}><span className={styles.statusMob}>{success ? "🟢" : "🔴" }</span> INR {price}</p>
-                    </div>
-                </div>
+const SingleTransaction = ({
+  success,
+  itemName,
+  transactionID,
+  date,
+  price,
+  nftUid,
+}: Transaction) => {
+  return (
+    <>
+      <div className={styles.mainDiv}>
+        <div className="d-flex  px-5 py-2 align-items-center">
+          {/* <img src="/images/pfImage.png" alt="" className={styles.pfImage} /> */}
+          <div
+            className={clsx(
+              "d-flex justify-content-between ",
+              styles.nameUsernamePrice
+            )}
+          >
+            <div className={styles.nameAndPrice}>
+              {itemName === "Creator Support" && (
+                <p className={styles.boldTitle}>{itemName}</p>
+              )}
+              {itemName === "NFT Purchase" && (
+                <Link to={`/nft/${nftUid}`}>
+                  <p className={styles.boldTitle}>{itemName}</p>
+                </Link>
+              )}
+              <p className={styles.boldTitle}>{transactionID}</p>
+              <p className={styles.date}>{date}</p>
             </div>
-        </>
-    )
-}
 
-export default SingleTransaction
+            {itemName === "Creator Support" && (
+              <p className={styles.boldTitle}>{itemName}</p>
+            )}
+            {itemName === "NFT Purchase" && (
+              <Link to={`/nft/${nftUid}`}>
+                <p className={styles.boldTitle}>{itemName}</p>
+              </Link>
+            )}
+            <p className={styles.dateDesk}>{date}</p>
+            <p className={styles.transDesk}>{transactionID}</p>
+            <li className={styles.statusDesk}>
+              {success === "success" ? "🟢 Success" : "🔴 Failed"}
+            </li>
+            <p className={styles.price}>
+              <span className={styles.statusMob}>
+                {success === "success" ? "🟢" : "🔴"}
+              </span>{" "}
+              INR {price}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleTransaction;
